3e7ee0b7e8f32a42c8e850fd7480206a4bdb7435
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-15 07:44:53 +0100

    Update TOKEN and CHAT_ID with hardcoded values
    Replaced environment variables for TOKEN and CHAT_ID with hardcoded values.

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import numpy as np
 # =========================
 # 📲 TELEGRAM
 # =========================
-TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+TOKEN = os.getenv("8439548325:AAHOBBHy7EwcX3J5neIaf6iJuSjyGJCuZ68")
+CHAT_ID = os.getenv("5067771509")
 
 def send_message(text):
     if not TOKEN or not CHAT_ID:
